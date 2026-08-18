500c77707d15d90b8bbba39203a987b06200e3cb
prepajar <51681437+prepajar@users.noreply.github.com>
2026-08-18 14:23:45 +0200

    Update fr_FR.h

--- src/lang/fr_FR.h
+++ src/lang/fr_FR.h
@@ -153,8 +153,7 @@
 #define D_HTTP_MAIN_MENU "Menu principal"
 #define D_HTTP_REBOOT "Redémarrer"
 #define D_HTTP_CONFIGURATION "Configuration"
-#define D_HTTP_CONFIG_CHANGED                                                                                            \
-    "La configuration a changé, cliquez sur <a href='/reboot'>Redémarrer</a> pour enregistrer les modifications dans " 
+#define D_HTTP_CONFIG_CHANGED "La configuration a changé, cliquez sur <a href='/reboot'>Redémarrer</a> pour enregistrer les modifications dans " 
 #define D_HTTP_SENDING_PAGE "La page %S envoyée à %s"
 #define D_HTTP_FOOTER "par Francis Van Roie"
 
