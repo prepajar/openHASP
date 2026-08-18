136d60125f8d657c5bdf935064d9afe147795fd7
prepajar <51681437+prepajar@users.noreply.github.com>
2026-08-18 14:10:03 +0200

    Update my_custom.h

--- src/custom/my_custom.h
+++ src/custom/my_custom.h
@@ -2,7 +2,7 @@
 #define HASP_CUSTOM_H
 #if defined(HASP_USE_CUSTOM)
 #include "hasplib.h"
-
+ 
 void custom_setup();
 void custom_loop();
 void custom_every_second();
@@ -10,6 +10,8 @@ void custom_every_5seconds();
 bool custom_pin_in_use(uint8_t pin);
 void custom_get_sensors(JsonDocument& doc);
 void custom_topic_payload(const char* topic, const char* payload, uint8_t source);
-
+void custom_state_subtopic(const char* subtopic, const char* payload);
+ 
 #endif // HASP_USE_CUSTOM
 #endif // HASP_CUSTOM_H
+ 
